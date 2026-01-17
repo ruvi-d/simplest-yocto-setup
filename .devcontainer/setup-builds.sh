d08f4408a63db1da66df8e7e8f890292d73204e9
Ruvinda Dhambarage <ruvinda.dhambarage@nagarro.com>
2026-01-17 16:12:38 +0000

    ti: add support to build meta-ti

--- .devcontainer/setup-builds.sh
+++ .devcontainer/setup-builds.sh
@@ -7,3 +7,5 @@ KAS_BUILD_DIR=build KAS_MACHINE=dogbonedark kas checkout .config.yaml:.devcontai
 KAS_BUILD_DIR=build-st KAS_MACHINE=stompduck kas checkout .config.yaml:.devcontainer.config.yml 
 
 KAS_BUILD_DIR=build-qemuarm KAS_MACHINE=qemuarm kas checkout .config.yaml:.devcontainer.config.yml
+
+KAS_BUILD_DIR=build-ti KAS_MACHINE=beaglebone kas checkout .config.ti-sdk.yaml:.devcontainer.config.yml
