d8be3e716b9d73f9fb0ff891dea1ea2d8efa7d81
Ruvinda Dhambarage <ruvinda.dhambarage@nagarro.com>
2026-01-17 09:44:40 -0500

    devcontainer: add script and task to create build folders
    add vscode ignore folder

---
+++ .devcontainer/setup-builds.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+set -e
+
+# Create build folders sequentially
+KAS_BUILD_DIR=build KAS_MACHINE=dogbonedark kas checkout .config.yaml:kas-devcontainer-configuration.yml
+
+KAS_BUILD_DIR=build-st KAS_MACHINE=stompduck kas checkout .config.yaml:kas-devcontainer-configuration.yml 
+
+KAS_BUILD_DIR=build-qemuarm KAS_MACHINE=qemuarm kas checkout .config.yaml:kas-devcontainer-configuration.yml
