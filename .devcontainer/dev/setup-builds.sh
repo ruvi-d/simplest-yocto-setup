89f6a33b7404a8db0feecdc7f69dd0a0ed68533e
Ruvinda Dhambarage <ruvinda.dhambarage@nagarro.com>
2026-04-29 23:22:13 +0000

    devcontainer: add stompgoose

--- .devcontainer/dev/setup-builds.sh
+++ .devcontainer/dev/setup-builds.sh
@@ -6,6 +6,8 @@ KAS_BUILD_DIR=build KAS_MACHINE=dogbonedark kas checkout .config.yaml:.config.de
 
 KAS_BUILD_DIR=build-st KAS_MACHINE=stompduck kas checkout .config.yaml:.config.devcontainer.yaml
 
+KAS_BUILD_DIR=build-st-custom KAS_MACHINE=stompgoose kas checkout .config.yaml:.config.devcontainer.yaml
+
 KAS_BUILD_DIR=build-qemuarm KAS_MACHINE=qemuarm kas checkout .config.yaml:.config.devcontainer.yaml
 
 KAS_BUILD_DIR=build-nxp KAS_MACHINE=freiheit93 kas checkout .config.yaml:.config.devcontainer.yaml:.config.nxp.eula.yaml
