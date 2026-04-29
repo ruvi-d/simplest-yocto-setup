34807cdcc17190285e8053f792b71ee542968107
Ruvinda Dhambarage <ruvinda.dhambarage@nagarro.com>
2026-04-29 22:25:09 +0000

    devcontainer: add dogbonedarker

--- .devcontainer/dev/setup-builds.sh
+++ .devcontainer/dev/setup-builds.sh
@@ -4,6 +4,8 @@ set -e
 # Create build folders sequentially
 KAS_BUILD_DIR=build KAS_MACHINE=dogbonedark kas checkout .config.yaml:.config.devcontainer.yaml
 
+KAS_BUILD_DIR=build-custom KAS_MACHINE=dogbonedarker kas checkout .config.yaml:.config.devcontainer.yaml
+
 KAS_BUILD_DIR=build-st KAS_MACHINE=stompduck kas checkout .config.yaml:.config.devcontainer.yaml
 
 KAS_BUILD_DIR=build-qemuarm KAS_MACHINE=qemuarm kas checkout .config.yaml:.config.devcontainer.yaml
